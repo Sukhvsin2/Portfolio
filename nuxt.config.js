84f75db1814bc670a4d4bbf4a7aa5d736a553ca6
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-03-23 17:36:59 -0500

    Name correction

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Sukhvin2',
+    titleTemplate: '%s - Sukhvsin2',
     title: 'Sukhvinder Singh',
     htmlAttrs: {
       lang: 'en'
